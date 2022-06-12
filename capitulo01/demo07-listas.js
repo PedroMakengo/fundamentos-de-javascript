272b5113ca2b4887d1490d6e9355554378ee2cf2
PedroMakengo <pedromakengomiguel@gmail.com>
2022-06-12 18:00:00 +0100

    Trabalhando na parte das listas

---
+++ capitulo01/demo07-listas.js
@@ -0,0 +1,22 @@
+const minhaLista = [];
+
+const minhaListaDeTarefas = [
+  "mandar email",
+  "colocar comida para o dog",
+  "limpar o quarto",
+];
+
+// console.log(minhaListaDeTarefas[0]);
+// console.log(minhaListaDeTarefas[1]);
+// console.log(minhaListaDeTarefas[4]);
+
+// Quantidade de itens no array
+// console.log(minhaListaDeTarefas.length);
+
+// adicionar item
+// minhaListaDeTarefas.push("formatar computador");
+// console.log(minhaListaDeTarefas);
+
+// remover ultimo da lista
+const ultimo = minhaListaDeTarefas.pop();
+console.log(ultimo, minhaListaDeTarefas);
