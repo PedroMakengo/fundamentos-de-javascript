9c6e226b40fbd5c6f4cefc9f4442b69662d474a4
PedroMakengo <pedromakengomiguel@gmail.com>
2022-06-12 19:30:46 +0100

    Trabalhando com objetos

---
+++ capitulo01/demo08-objetos.js
@@ -0,0 +1,13 @@
+// ### Trabalhando com Objetos
+// Pesquisar uma definição para o objetos...
+// Uma forma simples de trabalhar com objetos no javascript é usando a seguinte notação
+// const pessoa = {
+//   id: 1,
+//   nome: "Pedro",
+//   idade: 22,
+//   sexo: "Masculino",
+// };
+
+// Eu posso obter as informações do meu objeto usando o seguinte:
+// pessoa[id] ou pessoa.id que vai pegar o id do meu objeto pessoa.
+// No javascript tudo herda de outros ou seja tudo é objeto...
