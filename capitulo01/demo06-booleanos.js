a4c41ca24bf24778b39327bc8dc5efcbc31963bb
PedroMakengo <pedromakengomiguel@gmail.com>
2022-06-12 12:51:35 +0100

    Aprendendo sobre os tipos booleanos

---
+++ capitulo01/demo06-booleanos.js
@@ -0,0 +1,22 @@
+const podeDirigir = true;
+if (podeDirigir) {
+  console.log("Ela pode dirigir");
+}
+
+const saldoEmConta = 1;
+
+if (!saldoEmConta) {
+  console.log("não tem saldo!");
+}
+
+const boolComString = "ae hackerzão!!";
+
+// força o valor a true ou false
+// de acordo com a tabela
+console.log("boolComString", !!boolComString);
+
+// negação
+console.log(!boolComString);
+
+// negação + forçar a bool
+console.log("negação", !!!boolComString);
