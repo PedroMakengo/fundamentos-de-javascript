c896f8edacd54c19396339cd72342e796c405c85
PedroMakengo <pedromakengomiguel@gmail.com>
2022-06-16 14:15:02 +0100

    Trabalhei no arquivo demo14.js aplicando os novos conhecimentos

---
+++ capitulo01/demo14-classes.js
@@ -0,0 +1,12 @@
+class Heroi {
+  atacar() {
+    console.log(`atacou !!`);
+  }
+  defender() {
+    console.log(`defendeu!!`);
+  }
+}
+
+const heroi = new Heroi();
+heroi.atacar();
+heroi.defender();
