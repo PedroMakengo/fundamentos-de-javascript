76aa1ec75916025e60d3b515b35335fd4053a656
PedroMakengo <pedromakengomiguel@gmail.com>
2022-06-16 08:13:33 +0100

    Trabalhando

---
+++ capitulo01/demo10-objetos-globais.js
@@ -0,0 +1,14 @@
+// Pegar as chaves do meu objeto
+console.log("keys", Object.keys({ nome: "Pedro" }));
+
+// Pegar os valores do objeto
+console.log("values", Object.values({ nome: "Pedro" }));
+
+// Classe especifica para manipular datas em milisegundos desde ano 1970 até atual
+console.log("now", Date.now());
+
+// Random retorna um numero randomico
+console.log("random", Math.random());
+
+// Global verificar funções pré criadas
+console.log("global", global);
