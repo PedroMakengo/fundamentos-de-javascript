4a252191fd0ffe4cc896904034b12705c52ef4a3
PedroMakengo <pedromakengomiguel@gmail.com>
2022-06-17 19:46:33 +0100

    Entendendo um pouco sobre classes

--- capitulo01/demo14-classes.js
+++ capitulo01/demo14-classes.js
@@ -10,3 +10,29 @@ class Heroi {
 const heroi = new Heroi();
 heroi.atacar();
 heroi.defender();
+
+// Criando uma class
+class Heroi2 {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  atacar() {
+    console.log(`O ${this.nome} atacou!!`);
+  }
+}
+
+const heroi2 = new Heroi2("Flash", 80);
+
+heroi2.atacar();
+
+class Heroi3 {
+  static obterAnoNascimento(idade) {
+    return 2022 - idade;
+  }
+}
+
+const anoNascimento = Heroi3.obterAnoNascimento(1999);
+
+console.log(anoNascimento);
