5eb94664152cd1c1da2fbd004c07103e081b7c92
PedroMakengo <pedromakengomiguel@gmail.com>
2022-06-19 12:37:33 +0100

    Trabalhando com estruturas de repetição, entendo a principal importância de usar o for, forin e forof

--- capitulo02/demo04-for-forin-forof.js
+++ capitulo02/demo04-for-forin-forof.js
@@ -1,4 +1,29 @@
-for (let index = 0; index <= 10; index++) {
-  const decisao = index % 2 === 0 ? "Par" : "Impar";
-  console.log(decisao);
+// for (let index = 0; index <= 10; index++) {
+//   const decisao = index % 2 === 0 ? "Par" : "Impar";
+//   console.log(`O número ${index} é ${decisao}`);
+// }
+
+const minhaListaDeTarfas = [
+  { id: parseInt(Math.random() * 10), nome: "Zezinho", superPoder: "Veloz" },
+  { id: Math.random(), nome: "Mariazinha", superPoder: "Super Força" },
+];
+
+for (let index = 0; index < minhaListaDeTarfas.length; index++) {
+  const item = minhaListaDeTarfas[index];
+  console.log(`
+    id: ${item.id}
+    nome: ${item.nome}
+  `);
+}
+
+// não precisa do contador
+
+for (const index in minhaListaDeTarfas) {
+  const item = minhaListaDeTarfas[index];
+  console.log("Nome", item.nome);
+}
+
+// não precisa usar o index
+for (const item of minhaListaDeTarfas) {
+  console.log("Nome", item.nome);
 }
